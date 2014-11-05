df4241573b86143cfca589843b50cd7a506b1ca4
Jeremy H <secretmojo@g-m-a-i-l.com>
2014-11-05 01:29:00 -0600

    Do it programmatically, better tweaks
    Prep for moving the AI tweaks elsewhere so they don’t take over the
    stock AIs. Also, tweaks are better.

--- ui/main/game/new_game/js/ai.js
+++ ui/main/game/new_game/js/ai.js
@@ -22,64 +22,7 @@ function ai_types() {
     } 
     */
 
-    return {
-        'Normal (Redenbacher)': {
-            percent_land: 0.65,
-            percent_air: 0.25,
-            percent_naval: 0.05,
-            percent_orbital: 0.05,
-            metal_drain_check: .113,
-            energy_drain_check: 1.25,
-            metal_demand_check: 1.33,
-            energy_demand_check: 1.40,
-            micro_type: 0,
-            go_for_the_kill: false,
-            priority_scout_metal_spots: false,
-            neural_data_mod: 1.3
-        },        
-        'Hard (Redenbacher)': {
-            percent_land: 0.65,
-            percent_air: 0.25,
-            percent_naval: 0.05,
-            percent_orbital: 0.05,
-            metal_drain_check: 0.093,
-            energy_drain_check: 1.05,
-            metal_demand_check: 1.13,
-            energy_demand_check: 1.20,
-            micro_type: 0,
-            go_for_the_kill: false,
-            priority_scout_metal_spots: true,
-            neural_data_mod: 1.2
-        },        
-        'Relentless (Redenbacher)': {
-            percent_land: 0.65,
-            percent_air: 0.25,
-            percent_naval: 0.05,
-            percent_orbital: 0.05,
-            metal_drain_check: 0.073,
-            energy_drain_check: 0.85,
-            metal_demand_check: 0.93,
-            energy_demand_check: 1.0,
-            micro_type: 1,
-            go_for_the_kill: true,
-            priority_scout_metal_spots: true,
-            neural_data_mod: 1.1
-        },        
-        'Absurd (Redenbacher)': {
-            percent_land: 0.65,
-            percent_air: 0.25,
-            percent_naval: 0.05,
-            percent_orbital: 0.05,
-            metal_drain_check: 0.053,
-            energy_drain_check: 0.65,
-            metal_demand_check: 0.73,
-            energy_demand_check: 0.8,
-            micro_type: 2,
-            go_for_the_kill: true,
-            priority_scout_metal_spots: true,
-            neural_data_mod: 1.0
-        },
-        
+    var vanilla_types = {
         'Normal': {
             percent_land: 0.65,
             percent_air: 0.25,
@@ -137,4 +80,28 @@ function ai_types() {
             neural_data_mod: 1.0
         },
     };
+
+    var Redenbacher_AIs = {};
+    var theName = "";
+    var settings;
+
+     for (var i in vanilla_types) {
+      //clone
+      settings = JSON.parse(JSON.stringify(vanilla_types[i]));
+  
+      // add Redenbacher version
+      theName = i + " (Redenbacher)";      
+      settings["metal_drain_check"] =  settings["metal_drain_check"] * .26;
+      settings["metal_demand_check"] =  settings["metal_demand_check"] * .33;
+      settings["energy_drain_check"] = settings["energy_drain_check"] * .26;
+      settings["energy_demand_check"] = settings["energy_demand_check"] * .33;
+      Redenbacher_AIs[theName] = settings;
+    }
+
+    for (var i in vanilla_types) {      
+      // keep the original if user forgets to turn mod off.
+      Redenbacher_AIs[i] = vanilla_types[i];
+    }
+    
+    return Redenbacher_AIs;
 }
